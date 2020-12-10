9e1b2b776683c4c4d35932612f56090b86de84b6
Halvor <stektpotet@gmail.com>
2020-12-10 05:40:23 +0100

    Added solution to test.sh

--- days/day-09/test.sh
+++ days/day-09/test.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 D=$(dirname $(realpath $0))
 
 
-
-echo "$D / --- Empty --- ❌";
-exit 1337;
+echo ""
+echo "--- Day 9: Encoding Error ---"
+$D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/day09.stektpotet.py
+echo ""
