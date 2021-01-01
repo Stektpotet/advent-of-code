55c2814ad4377c643bf070db55115d51c679b489
Halvor Smedås <stektpotet@gmail.com>
2021-01-01 16:54:42 +0100

    Re-added part 1 to the solution of day14

--- days/day-14/solutions/stektpotet.py
+++ days/day-14/solutions/stektpotet.py
@@ -1,46 +1,56 @@
-import sys
+from sys import stdin
 import itertools
 
-ip = "mask = 000000000000000000000000000000X1001X\nmem[42] = 100\nmask = 00000000000000000000000000000000X0XX\nmem[26] = 1".split(
-    '\n')
-# ip = sys.stdin.read().split('\n')[:-1]
-print(ip[-1])
+if __name__ == '__main__':
+    program = stdin.readlines()
 
-instructions = []
-current_mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-for s in ip:
-    print(s)
-    if s[1] == 'a':
-        current_stack = []
-        current_mask = s.split()[-1]
-        instructions.append((current_mask, current_stack))
-        print(current_mask)
-    else:
-        current_stack.append((f"{int(s.split(']')[0][4:]):036b}", f"{int(s.split('=')[-1]):036b}"))
-print()
-print(instructions)
-print()
+    # === Part 1 ===
+    instructions = []
+    for line in program:
+        if line[1] == 'a':  # Is this line defining a mask?
+            instructions.append((line.split()[-1], []))
+        else:
+            instructions[-1][1].append((int(line.split(']')[0][4:]), f"{int(line.split('=')[-1]):036b}"))
 
-mem = dict()
-for m, ops in instructions:
-    for pos, v in ops:
-        xs = []
-        addr = [c for c in m]
-        for i, p in enumerate(pos):
-            if m[i] == '0':
-                addr[i] = p
-            elif m[i] == 'X':
-                xs.append(i)
-        print(''.join(addr))
-        addrs = []
-        for p in itertools.product('01', repeat=len(xs)):
-            for i, xi in enumerate(xs):
-                addr[xi] = p[i]
-            addrs.append(int(''.join(addr), 2))
+    mem = dict()
+    for m, ops in instructions:
+        for pos, v in ops:
+            vs = []
+            for i, x in enumerate(v):
+                c = m[i - len(v)]
+                vs.append(x if c == 'X' else c)
+            val = ''.join(vs)
+            mem[pos] = int(val, 2)
+    print(sum(mem.values()))
 
-        for a in addrs:
-            mem[a] = int(v, 2)
-        print(pos, int(pos, 2), addrs)
-        print()
+    # === Part 2 ===
 
-print(sum(mem.values()))
+    instructions.clear()
+    mem.clear()
+
+    for line in program:
+        if line[1] == 'a':  # Is this line defining a mask?
+            instructions.append((line.split()[-1], []))
+        else:
+            instructions[-1][1].append((f"{int(line.split(']')[0][4:]):036b}", f"{int(line.split('=')[-1]):036b}"))
+
+    for m, ops in instructions:
+        for pos, v in ops:
+            xs = []
+            addr = [c for c in m]
+            for i, p in enumerate(pos):
+                if m[i] == '0':
+                    addr[i] = p
+                elif m[i] == 'X':
+                    xs.append(i)
+            addrs = []
+            for p in itertools.product('01', repeat=len(xs)):
+                for i, xi in enumerate(xs):
+                    addr[xi] = p[i]
+                addrs.append(int(''.join(addr), 2))
+
+            for a in addrs:
+                mem[a] = int(v, 2)
+
+    print(sum(mem.values()))
+
